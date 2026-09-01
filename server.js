c06a82c77a036b8cad70400110ee352000d07a7d
lawrencelicos2 <lawrencelicos2@gmail.com>
2026-09-01 21:15:55 +1000

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
